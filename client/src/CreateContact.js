75a96117feb024a4a84735b8602b00fae46d62c5
Paulo Melo <peimelo@gmail.com>
2018-04-04 09:11:03 -0300

    server and client

--- client/src/CreateContact.js
+++ client/src/CreateContact.js
@@ -5,11 +5,11 @@ import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
   handleSubmit = (e) => {
-    e.preventDefault()
-    const values = serializeForm(e.target, { hash: true })
+    e.preventDefault();
+    const values = serializeForm(e.target, { hash: true });
     if (this.props.onCreateContact)
       this.props.onCreateContact(values)
-  }
+  };
 
   render() {
     return (
